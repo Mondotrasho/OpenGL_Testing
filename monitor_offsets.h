ec4e7c7f89d40bcf5cd7320f36fc2c7c1efd5041
Oscar Carter-South <13194805+Mondotrasho@users.noreply.github.com>
2026-06-24 10:35:58 +1000

    add monitor offsets cofiguration

---
+++ monitor_offsets.h
@@ -0,0 +1,13 @@
+
+// monitor_offsets.h
+#pragma once
+
+namespace Monitor {
+    constexpr int LEFT_X   = 0;     // Windows monitor 3, 1920x1080
+    constexpr int CENTER_X = 1920;  // Windows monitor 1, 3440x1440
+    constexpr int RIGHT_X  = 5360;  // Windows monitor 2, 2560x1440
+
+    constexpr int TOP_Y = 0;
+
+    constexpr int DEV_OFFSET_Y = 100;
+}
